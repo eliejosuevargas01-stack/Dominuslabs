2fde1b50d0914782577e78cbd30509a72ccfdb81
eliezer josue vargas <eliejosuevargas01@gmail.com>
2026-06-09 21:40:45 -0300

    feat: add support for external PostgreSQL deployment and add db_test script

--- project-hub/backend/app/core/database.py
+++ project-hub/backend/app/core/database.py
@@ -3,8 +3,11 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 from app.core.config import settings
 
+is_sqlite = settings.SQLALCHEMY_DATABASE_URI.startswith("sqlite")
+connect_args = {"check_same_thread": False} if is_sqlite else {}
+
 engine = create_engine(
-    settings.SQLALCHEMY_DATABASE_URI, connect_args={"check_same_thread": False}
+    settings.SQLALCHEMY_DATABASE_URI, connect_args=connect_args
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
